a2c179a8b468b53e8a8c86a6c8f80c9513fa4aa4
Jeroen Tuinstra <jeroen.tuinstra@rabobank.com>
2022-07-01 18:03:18 +0200

    added a profile and env reload after installing chocolatey

--- choco-init.ps1
+++ choco-init.ps1
@@ -2,12 +2,15 @@
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; 
 
-$packageUrl="https://community.chocolatey.org/install.ps1"
+$packageUrl="https://community.chocolatey.org/install.ps1";
 
 ## Create Powershell Profile if not exists
 if (!(Test-Path -Path $PROFILE)) {
-  New-Item -ItemType File -Path $PROFILE -Force
+  New-Item -ItemType File -Path $PROFILE -Force;
 }
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($packageUrl))
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($packageUrl));
+
+. $PROFILE;
+refreshenv;
